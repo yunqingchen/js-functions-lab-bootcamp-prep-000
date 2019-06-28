84067f283b8454dde12449973a71790be01363c7
yyunqingchenn <yyunqingchenn@gmail.com>
2019-06-28 06:15:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function happyHolidayTo(holiday, name)
   
 function holidayCountdown(holiday, days)
   {
-    return 'It\s ${days} days until ${holiday}!';
+    return 'It\'s ${days} days until ${holiday}!';
   }
